019fd52a52449cdb7e21717211280c4c638677bf
achliopa <athchliopanos@gmail.com>
2017-11-04 21:22:09 +0200

    up to lecture 54. all webpack set for development, going to clean app.js for refactoring

---
+++ indecision-app/src/person.js
@@ -0,0 +1,11 @@
+console.log('person.js is running');
+
+// const isAdult = (age) => age>=18;
+// const canDrink = (age) => age>=17;
+
+// export { isAdult, canDrink };
+
+export const isAdult = (age) => age>=18;
+export const canDrink = (age) => age>=17;
+
+export default (age) => age>=65;
