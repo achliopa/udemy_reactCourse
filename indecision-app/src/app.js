548ea3d7f365f8fd2c2f02b575a1ff7fc4ff1162
achliopa <athchliopanos@gmail.com>
2017-11-05 00:34:39 +0200

    finished section 6

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,31 +1,33 @@
-// // NAMED - DEFAULT FILE IMPORTS 
-
-// // import './utils.js'
-// import anythingIwant, { square, add } from './utils.js'
-
-
-// console.log('app.js is running');
-
-// console.log(square(3));
-// console.log(add(3,4));
-// console.log(anythingIwant(9,5));
-
-// import isSenior, { isAdult, canDrink } from './person.js'
-// console.log(isAdult(18));
-// console.log(canDrink(14));
-// console.log(isSenior(65));
-
-// Install -> Import -> Use
-
-// import validator from 'validator';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-// const someString = 'sakis:c@fdfd.com'
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
-// console.log(`${someString} is email? ${validator.isEmail(someString)}`);
 
-// it will break . needs babel to translate
- const template = <p>Testing 12344</p>;
-//const template = React.createElement('p', {}, 'testing 123');
-ReactDOM.render(template, document.getElementById('app'));
+// class OldSyntax {
+//   constructor() {
+//     this.name = 'Mike';
+//     this.getGreeting = this.getGreeting.bind(this);
+//   }
+//   getGreeting() {
+//     return `Hi. My name is ${this.name}`;
+//   }
+// }
+
+// const oldSyntax = new OldSyntax();
+// const getGreeting = oldSyntax.getGreeting;
+// console.log(getGreeting());
+
+// // ----------------------------
+
+// class NewSyntax {
+//   name = 'Jen';
+//   getGreeting = () => {
+//     return `Hi. My name is ${this.name}`;
+//   };
+// }
+
+// const newSyntax = new NewSyntax();
+// const newGetGreeting = newSyntax.getGreeting;
+// console.log(newGetGreeting());
