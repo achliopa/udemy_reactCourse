6c35449a6e0b3f82641ab4c38ddf9150c0884242
Sakis Chliopanos <athchliopanos@gmail.com>
2017-11-15 14:40:22 +0000

    finished section 11

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
 // const date = new Date();
 
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
+
 export default class ExpenseForm extends React.Component {
-	state = {
-		description: '',
-		note: '',
-		amount: ''
-	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			description: props.expense ? props.expense.description : '',
+			note: props.expense ? props.expense.note : '',
+			amount: props.expense ? (props.expense.amount / 100).toString() : '',
+			createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+			calendarFocused: false,
+			error: ''
+		};
+	}
+	
 	onDescriptionChange = (e) => {
 		const description = e.target.value;
 		this.setState(() => ({ description }));
@@ -21,15 +33,43 @@ export default class ExpenseForm extends React.Component {
 
 	onAmountChange = (e) => {
 		const amount = e.target.value;
-		if( amount.match(/^\d*(\.\d{0,2})?$/)) {
+		if(!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
 			this.setState(() => ({ amount }));
 		}
 	};
-
+	
+	onDateChange = (createdAt) => {
+		if(createdAt) {
+			this.setState(() => ({ createdAt }));
+		}	
+	};
+	
+	onFocusChange = ({ focused }) => {
+		this.setState(() => ({ calendarFocused: focused }));
+	};
+	
+	onSubmit = (e) => {
+		e.preventDefault();
+		
+		if (!this.state.description || !this.state.amount) {
+			// set error state equal to 'Please provide description and amount'
+			this.setState(() => ({ error: 'Please provide description and amount'}));
+		} else {
+			this.setState(() => ({ error: ''}));
+			this.props.onSubmit({
+				description: this.state.description,
+				amount: parseFloat(this.state.amount,10) * 100,
+				createdAt: this.state.createdAt.valueOf(),
+				note: this.state.note
+			});
+		}
+	};
+	
 	render() {
 		return (
 			<div>
-				<form>
+				{this.state.error && <p>{this.state.error}</p>}
+				<form onSubmit={this.onSubmit}>
 					<input 
 						type='text'
 						placeholder='Description'
@@ -42,6 +82,14 @@ export default class ExpenseForm extends React.Component {
 						placeholder='Amount'
 						value={this.state.amount}
 						onChange={this.onAmountChange}
+					/>
+					<SingleDatePicker
+						date={this.state.createdAt} // momentPropTypes.momentObj or null
+						onDateChange={this.onDateChange} // PropTypes.func.isRequired
+						focused={this.state.calendarFocused} // PropTypes.bool
+						onFocusChange={this.onFocusChange} // PropTypes.func.isRequired
+						numberOfMonths={1}
+						isOutsideRange={() => false}
 					/>
 					<textarea 
 						placeholder='Add a note for your expense (optional)'
--- expensify-app/src/components/ExpenseListFilters.js
+++ expensify-app/src/components/ExpenseListFilters.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter, sortByDate, sortByAmount } from '../actions/filters'
-
-const ExpenseListFilters = (props) => (
-	<div>
-		<input type='text' value={props.filters.text} onChange={(e) => {
-			props.dispatch(setTextFilter(e.target.value));
-		}}/>
-		<select value={props.filters.sortBy} onChange={(e) => {
-			if(e.target.value==='date') {
-				props.dispatch(sortByDate());
-			} else if (e.target.value==='amount') {
-				props.dispatch(sortByAmount());
-			} 
-		}}>
-			<option value='date'>Date</option>
-			<option value='amount'>Amount</option>
-		</select>
-	</div>
-);
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters'
+import { DateRangePicker } from 'react-dates';
+class ExpenseListFilters extends React.Component {
+	state = {
+		calendarFocused: null
+	};
+	
+	onDatesChange = ({startDate, endDate}) => {
+		this.props.dispatch(setStartDate(startDate));
+		this.props.dispatch(setEndDate(endDate));
+	};
+	
+	onFocusChange = (calendarFocused) => {
+		this.setState(() => ({ calendarFocused}));
+	};
+	
+	render () {
+		return (
+			<div>
+				<input type='text' value={this.props.filters.text} onChange={(e) => {
+					this.props.dispatch(setTextFilter(e.target.value));
+				}}/>
+				<select value={this.props.filters.sortBy} onChange={(e) => {
+					if(e.target.value==='date') {
+						this.props.dispatch(sortByDate());
+					} else if (e.target.value==='amount') {
+						this.props.dispatch(sortByAmount());
+					} 
+				}}>
+					<option value='date'>Date</option>
+					<option value='amount'>Amount</option>
+				</select>
+				<DateRangePicker
+					startDate={this.props.filters.startDate} // momentPropTypes.momentObj or null,
+					endDate={this.props.filters.endDate} // momentPropTypes.momentObj or null,
+					onDatesChange={this.onDatesChange} // PropTypes.func.isRequired,
+					focusedInput={this.state.calendarFocused} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+					onFocusChange={this.onFocusChange} // PropTypes.func.isRequired,
+					showClearDates={true}
+					numberOfMonths={1}
+					isOutsideRange={()=> false}
+				/>
+			</div>	
+		);
+	}
+};
 
 const mapStateToProps = (state) => ({
 	filters: state.filters
